ca9185379b6c7a2a61e49f4d3e07511d503bb40a
Yuki Yugui Sonoda <yugui@yugui.jp>
2018-02-14 10:05:29 +0900

    Add a C++ implementation of the server

---
+++ cpp/server_main.cc
@@ -0,0 +1,42 @@
+#include <gflags/gflags.h>
+#include <grpc++/grpc++.h>
+
+#include "greeter.grpc.pb.h"
+
+DEFINE_string(addr, "0.0.0.0:5000", "addr to listen on");
+
+namespace greeter {
+using grpc::Status;
+using grpc::ServerContext;
+
+class GreeterSerivceImpl final : public Greeter::Service {
+  Status Greet(ServerContext* context, const RequestProto* request,
+               ResponseProto* reply) override;
+};
+
+Status GreeterSerivceImpl::Greet(ServerContext* context,
+                                 const RequestProto* request,
+                                 ResponseProto* reply) {
+  reply->set_message("Hello, " + request->name());
+  return Status::OK;
+}
+}  // namespace greeter
+
+void RunService() {
+  greeter::GreeterSerivceImpl service;
+  grpc::ServerBuilder builder;
+
+  builder.AddListeningPort(FLAGS_addr, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+
+  const auto server(builder.BuildAndStart());
+  std::cerr << "Listening on " << FLAGS_addr << std::endl;
+
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  RunService();
+  return 0;
+}
